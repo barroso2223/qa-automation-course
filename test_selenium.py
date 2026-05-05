c46acaa5dee20865b2fe6f6d6184c0a0e2809204
barroso2223 <barroso8292@gmail.com>
2026-05-05 14:11:55 -0400

    Add headless Chrome and fix workflow

--- test_selenium.py
+++ test_selenium.py
@@ -9,7 +9,11 @@ import time
 
 @pytest.fixture
 def driver():
-    drive = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    drive = webdriver.Chrome(options=options)
     yield drive
     drive.quit()
 
